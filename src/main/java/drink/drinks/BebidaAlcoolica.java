0c543c062a54d18e03dc20664174cf5de882b3d2
biancalr <33333728+biancalr@users.noreply.github.com>
2018-05-10 18:51:09 -0300

    Reiniciado

---
+++ src/main/java/drink/drinks/BebidaAlcoolica.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package drink.drinks;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+
+/**
+ *
+ * @author Bianca
+ */
+@Entity
+@DiscriminatorValue(value = "SIM")
+public class BebidaAlcoolica extends Bebida implements Serializable {
+    
+    @Column(name = "PERCENT_TEOR_ALCOOL", nullable = true)
+    private Float teor;
+
+    public Float getTeor() {
+        return teor;
+    }
+
+    public void setTeor(Float teor) {
+        this.teor = teor;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(super.toString());
+        sb.append(" Alcoólica: SIM\n Percentual Teor Alcoólico:");
+        sb.append(this.teor);
+        return sb.toString();
+    }
+    
+    
+    
+    
+}
