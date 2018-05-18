5c22cddddcc9d873951e4577dca430cb3038090c
biancalr <33333728+biancalr@users.noreply.github.com>
2018-05-17 21:30:14 -0300

    Classe Teste para testar a classe Cliente

---
+++ src/test/java/test/com/mycompany/TesteCliente.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com.mycompany;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Bianca
+ */
+public class TesteCliente {
+    
+    private static EntityManagerFactory emf;
+    private static Logger logger;
+    private EntityManager em;
+    private EntityTransaction et;
+    
+    @BeforeClass
+    public static void setUpClass() {
+        logger = Logger.getGlobal();
+        //logger.setLevel(Level.INFO);
+        logger.setLevel(Level.SEVERE);
+        emf = Persistence.createEntityManagerFactory("drinkPU");
+        DbUnitUtil.inserirDados();
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+        emf.close();
+    }
+    
+    @Before
+    public void setUp() {
+        em = emf.createEntityManager();
+        beginTransaction();
+    }
+    
+    @After
+    public void tearDown() {
+        commitTransaction();
+        em.close();
+    }
+
+    private void beginTransaction() {
+        et = em.getTransaction();
+        et.begin();
+    }
+
+    private void commitTransaction() {
+        try {
+            et.commit();
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, ex.getMessage(), ex);
+            et.rollback();
+            fail(ex.getMessage());
+        }
+    }
+    
+    public void t01_persistirClienteValido(){
+        //TODO
+    }
+    
+}
